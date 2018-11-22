a3dbdd40b207147c0579aa582d272ed259fdaecf
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2018-11-21 23:20:47 -0500

    Concepto de variables

---
+++ variables/src/variables.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    //Tipos de variables
+
+    //Mutables
+    var mutables = 2
+
+
+    //Inmutables
+    val inmutables = 6
+
+    //implementacion de los diferentes tipos
+    mutables = 3
+
+    inmutables = 4
+
+    print(mutables + inmutables)
+
+}
